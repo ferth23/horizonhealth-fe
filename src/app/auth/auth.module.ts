68a82c60577df886acd12d36a902538e81b4c4c2
ferth23 <torresherrerafernanda1@gmail.com>
2024-10-10 22:13:04 -0600

    login y register

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LogInPageComponent } from './pages/log-in-page/log-in-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
@@ -11,7 +12,8 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
---
+++ src/app/auth/validators/validators.ts
@@ -0,0 +1,4 @@
+
+export const firstNameAndLastNamePattern: string = '^(?:\w+\s?){1,5}$';
+
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
