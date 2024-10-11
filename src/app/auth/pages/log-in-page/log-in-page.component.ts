29db60372d9685b943beb9c2b991b6dcb086e13a
Humberto-MS <humberto.ms123@gmail.com>
2024-10-11 11:27:23 -0600

    agregados a los forms

--- src/app/auth/pages/log-in-page/log-in-page.component.ts
+++ src/app/auth/pages/log-in-page/log-in-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as customValidators from '../../validators/validators';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-log-in-page',
@@ -8,18 +10,21 @@ import * as customValidators from '../../validators/validators';
   styleUrls: ['./log-in-page.component.css']
 })
 export class LogInPageComponent {
+  private fb = inject ( FormBuilder );
+  private router = inject ( Router );
+
   public myForm: FormGroup = this.fb.group ({
     email: [ '', [ Validators.required, Validators.pattern ( customValidators.emailPattern ) ] ],
     password: [ '', [ Validators.required, Validators.minLength ( 10 ) ] ],
-  })
+  });
 
-  constructor ( private fb: FormBuilder ) { }
+  login () {
+    const { email, password } = this.myForm.value;
 
-  isValidField ( field: string ) {
-    // TODO
-  }
-
-  onSubmit () {
-    this.myForm.markAllAsTouched ();
+    // this.authService.login ( email, password )
+    //   .subscribe ( {
+    //     next: () => this.router.navigateByUrl ( 'horizon-health' ),
+    //     error: ( message => Swal.fire ( 'Error', message, 'error' ) )
+    //   } );
   }
 }
--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as customValidators from '../../validators/validators';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-register-page',
@@ -8,20 +10,23 @@ import * as customValidators from '../../validators/validators';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+  private fb = inject ( FormBuilder );
+  private router = inject ( Router );
+
   public myForm: FormGroup = this.fb.group ({
     fullName: [ '', [ Validators.required, Validators.pattern ( customValidators.firstNameAndLastNamePattern ) ]] ,
     email: [ '', [ Validators.required, Validators.pattern ( customValidators.emailPattern ) ] ],
     password: [ '', [ Validators.required, Validators.minLength ( 10 ) ] ],
     confirmPassword: [ '', [ Validators.required ] ]
-  })
+  });
 
-  constructor ( private fb: FormBuilder ) { }
+  register () {
+    const { name, email, password } = this.myForm.value;
 
-  isValidField ( field: string ) {
-    // TODO
-  }
-
-  onSubmit () {
-    this.myForm.markAllAsTouched ();
+    // this.authService.register ( name, email, password )
+    //   .subscribe ( {
+    //     next: () => this.router.navigateByUrl ( 'horizon-health' ),
+    //     error: ( message => Swal.fire ( 'Error', message, 'error' ) )
+    //   } );
   }
 }
