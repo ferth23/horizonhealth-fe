c8092346adb50df83f53b03e22ffa43f0aa2b514
María Torres <142964930+ferth23@users.noreply.github.com>
2024-10-10 22:55:38 -0600

    login y register (#18)

--- src/app/auth/pages/log-in-page/log-in-page.component.ts
+++ src/app/auth/pages/log-in-page/log-in-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../validators/validators';
 
 @Component({
   selector: 'app-log-in-page',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./log-in-page.component.css']
 })
 export class LogInPageComponent {
+  public myForm: FormGroup = this.fb.group ({
+    email: [ '', [ Validators.required, Validators.pattern ( customValidators.emailPattern ) ] ],
+    password: [ '', [ Validators.required, Validators.minLength ( 10 ) ] ],
+  })
 
+  constructor ( private fb: FormBuilder ) { }
+
+  isValidField ( field: string ) {
+    // TODO
+  }
+
+  onSubmit () {
+    this.myForm.markAllAsTouched ();
+  }
 }
--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group ({
+    fullName: [ '', [ Validators.required, Validators.pattern ( customValidators.firstNameAndLastNamePattern ) ]] ,
+    email: [ '', [ Validators.required, Validators.pattern ( customValidators.emailPattern ) ] ],
+    password: [ '', [ Validators.required, Validators.minLength ( 10 ) ] ],
+    confirmPassword: [ '', [ Validators.required ] ]
+  })
 
+  constructor ( private fb: FormBuilder ) { }
+
+  isValidField ( field: string ) {
+    // TODO
+  }
+
+  onSubmit () {
+    this.myForm.markAllAsTouched ();
+  }
 }
