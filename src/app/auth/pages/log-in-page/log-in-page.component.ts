3a2b9914f21b7d3d4a4341ac0eadfdce9cc1b62e
María Torres <142964930+ferth23@users.noreply.github.com>
2024-10-11 11:31:06 -0600

    carousel height (#20)

--- src/app/auth/pages/log-in-page/log-in-page.component.ts
+++ src/app/auth/pages/log-in-page/log-in-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as customValidators from '../../validators/validators';
 
@@ -8,6 +8,14 @@ import * as customValidators from '../../validators/validators';
   styleUrls: ['./log-in-page.component.css']
 })
 export class LogInPageComponent {
+  @ViewChild ( 'eyeopened', { static: true } )
+  eye_opened!: ElementRef<SVGElement>;
+
+  @ViewChild ( 'eyeclosed', { static: true } )
+  eye_closed!: ElementRef<SVGElement>;
+
+  public type: string = 'password';
+
   public myForm: FormGroup = this.fb.group ({
     email: [ '', [ Validators.required, Validators.pattern ( customValidators.emailPattern ) ] ],
     password: [ '', [ Validators.required, Validators.minLength ( 10 ) ] ],
@@ -22,4 +30,16 @@ export class LogInPageComponent {
   onSubmit () {
     this.myForm.markAllAsTouched ();
   }
+
+  public togglePasswordView(): void {
+    if ( this.eye_opened.nativeElement.classList.contains('invisible') ) {
+      this.eye_opened.nativeElement.classList.remove('invisible');
+      this.eye_closed.nativeElement.classList.add('invisible');
+      this.type = 'text';
+    } else {
+      this.eye_opened.nativeElement.classList.add('invisible');
+      this.eye_closed.nativeElement.classList.remove('invisible');
+      this.type = 'password';
+    }
+  }
 }
