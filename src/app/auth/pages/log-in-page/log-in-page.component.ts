4e93a8756b75383dbab5437f1b03c615d81b7de3
ferth23 <torresherrerafernanda1@gmail.com>
2024-10-12 23:40:21 -0600

    no entiendo

--- src/app/auth/pages/log-in-page/log-in-page.component.ts
+++ src/app/auth/pages/log-in-page/log-in-page.component.ts
@@ -1,31 +1,50 @@
+/* ----------------------------------------------------------------------------
+ * HorizonHealth
+ *
+ * Archivo       : log-in-page.components.ts
+ * Autor         : María Fernanda Torres Herrera
+ * Fecha         : 07/10/2024
+ * Descripción   : Lógica de la página de inicio de sesión de Horizon Health
+ *
+ * Modificaciones:
+ * Fecha         Modificado por            Descripción
+ * 08/10/2024    María Torres Herrera      Se declaró el formulario de inicio de
+ *                                         sesión y se añadió una validación para
+ *                                         la contraseña
+ *
+ * 11/10/2024    Humberto Medina Santos    Se añadió el método 'login()'
+ * ---------------------------------------------------------------------------- */
+
 import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as customValidators from '../../validators/validators';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2'
 
-@Component({
+@Component ( {
   selector: 'app-log-in-page',
   templateUrl: './log-in-page.component.html',
   styleUrls: ['./log-in-page.component.css']
-})
+} )
 export class LogInPageComponent {
   private fb = inject ( FormBuilder );
   private router = inject ( Router );
 
-  @ViewChild ( 'eyeopened', { static: true } )
-  eye_opened!: ElementRef<SVGElement>;
+  // @ViewChild ( 'eyeopened', { static: true } )
+  // eye_opened!: ElementRef<SVGElement>;
 
-  @ViewChild ( 'eyeclosed', { static: true } )
-  eye_closed!: ElementRef<SVGElement>;
+  // @ViewChild ( 'eyeclosed', { static: true } )
+  // eye_closed!: ElementRef<SVGElement>;
 
   public type: string = 'password';
 
-  public myForm: FormGroup = this.fb.group ({
+  // * Definición del formulario de inicio de sesión
+  public myForm: FormGroup = this.fb.group ( {
     email: [ '', [ Validators.required, Validators.pattern ( customValidators.emailPattern ) ] ],
     password: [ '', [ Validators.required, Validators.minLength ( 10 ) ] ],
-  });
+  } );
 
+  // * Método para validar el inicio de sesión, sin terminar
   login () {
     const { email, password } = this.myForm.value;
 
@@ -36,15 +55,15 @@ export class LogInPageComponent {
     //   } );
   }
 
-  public togglePasswordView(): void {
-    if ( this.eye_opened.nativeElement.classList.contains('invisible') ) {
-      this.eye_opened.nativeElement.classList.remove('invisible');
-      this.eye_closed.nativeElement.classList.add('invisible');
-      this.type = 'text';
-    } else {
-      this.eye_opened.nativeElement.classList.add('invisible');
-      this.eye_closed.nativeElement.classList.remove('invisible');
-      this.type = 'password';
-    }
-  }
+  // public togglePasswordView(): void {
+  //   if ( this.eye_opened.nativeElement.classList.contains('invisible') ) {
+  //     this.eye_opened.nativeElement.classList.remove('invisible');
+  //     this.eye_closed.nativeElement.classList.add('invisible');
+  //     this.type = 'text';
+  //   } else {
+  //     this.eye_opened.nativeElement.classList.add('invisible');
+  //     this.eye_closed.nativeElement.classList.remove('invisible');
+  //     this.type = 'password';
+  //   }
+  // }
 }
--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,25 +1,44 @@
+/* ----------------------------------------------------------------------------
+ * HorizonHealth
+ *
+ * Archivo       : register-page.components.ts
+ * Autor         : María Fernanda Torres Herrera
+ * Fecha         : 07/10/2024
+ * Descripción   : Lógica de la página de registro de Horizon Health
+ *
+ * Modificaciones:
+ * Fecha         Modificado por            Descripción
+ * 08/10/2024    María Torres Herrera      Se declaró el formulario de registro y
+ *                                         se añadieron validaciones para el nombre
+ *                                         y la contraseña
+ *
+ * 11/10/2024    Humberto Medina Santos    Se añadió el método 'register()'
+ * ---------------------------------------------------------------------------- */
+
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as customValidators from '../../validators/validators';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2'
 
-@Component({
+@Component( {
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
-})
+} )
 export class RegisterPageComponent {
   private fb = inject ( FormBuilder );
   private router = inject ( Router );
 
-  public myForm: FormGroup = this.fb.group ({
+  // * Definición del formulario de registro
+  public myForm: FormGroup = this.fb.group ( {
     fullName: [ '', [ Validators.required, Validators.pattern ( customValidators.firstNameAndLastNamePattern ) ]] ,
     email: [ '', [ Validators.required, Validators.pattern ( customValidators.emailPattern ) ] ],
     password: [ '', [ Validators.required, Validators.minLength ( 10 ) ] ],
     confirmPassword: [ '', [ Validators.required ] ]
-  });
+  } );
 
+  // * Método para validar el registro, sin terminar
   register () {
     const { name, email, password } = this.myForm.value;
 
