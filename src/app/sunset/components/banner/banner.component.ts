c6ad928061b74e9f0e626316f21574e2822adb33
María Torres <142964930+ferth23@users.noreply.github.com>
2024-10-12 23:24:51 -0600

    documentación de banner, login y register (#26)
    * commit random pq no me deja hacer pull en main
    
    * comentarios cheverengues

--- src/app/sunset/components/banner/banner.component.ts
+++ src/app/sunset/components/banner/banner.component.ts
@@ -1,15 +1,33 @@
+/* ----------------------------------------------------------------------------
+ * HorizonHealth
+ *
+ * Archivo       : banner.components.ts
+ * Autor         : María Fernanda Torres Herrera
+ * Fecha         : 01/10/2024
+ * Descripción   : Lógica del componente Banner
+ *
+ * Modificaciones:
+ * Fecha         Modificado por            Descripción
+ * 04/10/2024    Humberto Medina Santos    Se añadió el método 'goToHorizon()', el
+ *                                         cual redirige a la página del proyecto
+ *                                         Horizon Health
+ *
+ * 12/10/2024    María Torres Herrera      Se añadieron espaciados
+ * ---------------------------------------------------------------------------- */
+
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
-@Component({
+@Component ( {
   selector: 'banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
-})
+  styleUrls: [ './banner.component.css' ]
+} )
 export class BannerComponent {
-  private router = inject(Router);
+  private router = inject ( Router );
 
-  public goToHorizon(): void {
-    this.router.navigateByUrl('horizon-health');
+  // * Método que redirige a la página del proyecto Horizon Health
+  public goToHorizon (): void {
+    this.router.navigateByUrl ( 'horizon-health' );
   }
 }
